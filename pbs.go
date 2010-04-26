6699c410fd015a92ee88bd20061f130eca8c5fe5
Michael Dietz <mdietz@mdietzimac.rice.edu>
2010-04-26 17:56:11 -0500

    Committing a broken build.  Memory store with LRU logic and basic multi-level store PBSDataStore objects are coming together

--- pbs.go
+++ pbs.go
@@ -5,7 +5,6 @@ import (
 	"goprotobuf.googlecode.com/hg/proto"
 	"crypto/sha256"
 	"encoding/hex"
-	"container/heap"
 )
 
 type DataStore interface{
@@ -19,6 +18,8 @@ type PBSStorage interface{
 	Get(key string, value interface{}) (err os.Error)
 	Delete(key string) (err os.Error)
 	Expire(key string) (err os.Error)
+	Next() (ds PBSDataStore)
+	Prev() (ds PBSDataStore)
 }
 
 type MemoryElem struct{
@@ -28,39 +29,36 @@ type MemoryElem struct{
 }	
 	
 type MemoryStore struct{
-	kvstore map[string]MemoryElem
+	kvstore map[string] *MemoryElem
 	mruKey string
 	lruKey string
 	maxBytes int64
-	PBSDataStore
 }
 
 type DiskStore struct{
 	filename string
 	maxBytes int64
-	PBSDataStore
 }
 
 type NetworkStore struct{
 	hostname string
 	maxBytes int64
-	PBSDataStore
 }
 
 type PBSDataStore struct{
-	keys map[string] PBSDataStore
-	DataStore
-	next PBSDataStore
-	prev PBSDataStore
+	keys map[string] *PBSDataStore
+	*DataStore
+	next *PBSDataStore
+	prev *PBSDataStore
 }
 
-func DoMarshalling(value interface{}) (key string, data byte[], err os.Error) {
-	data, err := proto.Marshal(value)
+func DoMarshalling(value interface{}) (key string, data []byte, err os.Error) {
+	data, err = proto.Marshal(value)
 	if err == nil {
 		h := sha256.New()
 		nr, err := h.Write([]byte(data))
 		if err == nil && nr == len([]byte(data)) {
-			key := hex.EncodeToString(h.Sum())
+			key = hex.EncodeToString(h.Sum())
 		}
 	}
 	return
@@ -69,16 +67,14 @@ func DoMarshalling(value interface{}) (key string, data byte[], err os.Error) {
 func NewPBSDataStore(stores []DataStore) *PBSDataStore {
 	ds := new(PBSDataStore)
 	ds.prev = nil
-	ds.DataStore = stores[0]
-	stores[0].PBSDataStore = ds
-	ds.keys = make(map[string] PBSDataStore)
-	prev = ds
-	for store in stores[1:] {
+	ds.DataStore = &stores[0]
+	ds.keys = make(map[string] *PBSDataStore)
+	prev := ds
+	for _, store := range stores[1:] {
 		ns := new(PBSDataStore)
 		prev.next = ns
 		ns.prev = prev
-		ns.DataStore = store
-		store.PBSDataStore = ns
+		ns.DataStore = &store
 		prev = ns
 	}
 	return ds
@@ -87,9 +83,9 @@ func NewPBSDataStore(stores []DataStore) *PBSDataStore {
 func (ds *PBSDataStore) Put(value interface{}) (key string, err os.Error) {
 	t_key, data, err := DoMarshalling(value)
 	if err == nil && ds.keys[t_key] == nil {
-		err = ds.PBSStore.Put(t_key, data)
+		err = ds.DataStore.Put(t_key, data)
 		if err == nil {
-			ds.keys[t_key] = ds.PBSStore
+			ds.keys[t_key] = ds
 			key = t_key
 		}
 	}
@@ -97,62 +93,92 @@ func (ds *PBSDataStore) Put(value interface{}) (key string, err os.Error) {
 }
 
 func (ds *PBSDataStore) Get(key string, value interface{}) (err os.Error) {
-	
+	return
 }
 
 func (ds *PBSDataStore) Delete(key string) (err os.Error) {
-	
+	return
 }
 
 func (ds *PBSDataStore) Push(key string) (err os.Error) {
-	
+	return
 }
 
 func (ds *PBSDataStore) Pull(key string) (err os.Error) {
-
+	return
 }
 
 func NewMemoryStore() *MemoryStore {
 	ds := new(MemoryStore)
-	ds.kvstore = make(map[string][]byte)
+	ds.kvstore = make(map[string] *MemoryElem)
+	ds.mruKey = ""
+	ds.lruKey = ""
 	return ds
 }
 
-func (ds *PBSMemoryStore) Put(value interface{}) (key string, err os.Error) {
-	data, err := proto.Marshal(value)
-	if err == nil {
-		h := sha256.New()
-		nr, err := h.Write([]byte(data))
-		if err == nil && nr == len([]byte(data)) {
-			t_key := hex.EncodeToString(h.Sum())
-			if ds.kvstore[t_key] == nil {
-				ds.kvstore[t_key] = data
-				key = t_key
-			} else {
-				err = os.NewError("Key already exists")
-			}
+func (ds *MemoryStore) Put(key string, data []byte) (err os.Error) {
+	if ds.kvstore[key] == nil {
+		if ds.mruKey == "" && ds.lruKey == "" {
+			ds.mruKey = key
+			ds.lruKey = key
+			elem := new(MemoryElem)
+			elem.data = data
+			elem.nextLRU = ""
+			elem.prevLRU = ""
+			ds.kvstore[key] = elem
+		} else if ds.kvstore[ds.mruKey] != nil && ds.kvstore[ds.mruKey].nextLRU == "" {
+			ds.kvstore[ds.mruKey].nextLRU = key
+			elem := new(MemoryElem)
+			elem.data = data
+			elem.nextLRU = ""
+			elem.prevLRU = ds.mruKey
+			ds.kvstore[key] = elem
+			ds.mruKey = key
+		} else {
+			err = os.NewError("Should never happen")
+		}
+	} else {
+		err = os.NewError("Key already exists")
+	}
+	return
+}
+
+func (ds *MemoryStore) Get(key string, value interface{}) (err os.Error) {
+	if ds.kvstore[key] != nil {
+
+		err = proto.Unmarshal(ds.kvstore[key].data, value)
+
+		if err == nil {
+
+			if ds.lruKey == key {
+				ds.lruKey = ds.kvstore[key].nextLRU
+				ds.kvstore[ds.mruKey].nextLRU = key
+				ds.kvstore[key].prevLRU = ds.mruKey
+				ds.mruKey = key
+			} else if ds.mruKey != key {
+				prev := ds.kvstore[key].prevLRU
+				next := ds.kvstore[key].nextLRU
+
+				ds.kvstore[prev].nextLRU = next
+				ds.kvstore[next].prevLRU = prev
+
+				ds.kvstore[ds.mruKey].nextLRU = key
+				ds.kvstore[key].prevLRU = ds.mruKey
+				ds.mruKey = key
+			} // Otherwise key == ds.mruKey, do nothing
 		}
 	}
 	return
 }
 
-func (ds *PBSMemoryStore) Get(key string, value interface{}) (err os.Error) {
-	if ds.kvstore[key] != nil {
-		err = proto.Unmarshal(ds.kvstore[key], value)
-	}
-	return
-}
-
-func NewPBSDiskStore(filename string) *PBSDiskStore {
-	ds := new(PBSDiskStore)
-	ds.keys = make([]string)
+func NewDiskStore(filename string) *DiskStore {
+	ds := new(DiskStore)
 	ds.filename = filename
 	return ds
 }
 
-func NewPBSNetworkStore(hostname string) *PBSNetworkStore {
-	ds := new(PBSNetworkStore)
-	ds.keys = make([]string)
+func NewNetworkStore(hostname string) *NetworkStore {
+	ds := new(NetworkStore)
 	ds.hostname = hostname
 	return ds
 }
