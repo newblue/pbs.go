62f664de3c88e64258f2b27cc0c36cf37f0f6c04
Michael Dietz <mdietz@gmail.com>
2010-04-26 04:47:05 -0500

    Restructuring pretty much everything

--- pbs.go
+++ pbs.go
@@ -5,61 +5,120 @@ import (
 	"goprotobuf.googlecode.com/hg/proto"
 	"crypto/sha256"
 	"encoding/hex"
+	"container/heap"
 )
 
-type PBSStore interface{
+type DataStore interface{
+	Put(key string, data []byte) (err os.Error)
+	Get(key string, data []byte) (err os.Error)
+	Delete(key string) (err os.Error)
+}
+
+type PBSStorage interface{
 	Put(value interface{}) (key string, err os.Error)
 	Get(key string, value interface{}) (err os.Error)
 	Delete(key string) (err os.Error)
-	Push(key string) (err os.Error)
-	Pull(key String) (err os.Error)
-}
-		
-type PBSMemoryStore struct{
-	kvstore map[string][]byte
+	Expire(key string) (err os.Error)
 }
 
-type PBSDiskStore struct{
-	keys []string
+type MemoryElem struct{
+	data []byte
+	nextLRU string
+	prevLRU string
+}	
+	
+type MemoryStore struct{
+	kvstore map[string]MemoryElem
+	mruKey string
+	lruKey string
+	maxBytes int64
+	PBSDataStore
+}
+
+type DiskStore struct{
 	filename string
+	maxBytes int64
+	PBSDataStore
 }
 
-type PBSNetworkStore struct{
-	keys []string
+type NetworkStore struct{
 	hostname string
+	maxBytes int64
+	PBSDataStore
 }
 
 type PBSDataStore struct{
-	stores []PBSStore
+	keys map[string] PBSDataStore
+	DataStore
+	next PBSDataStore
+	prev PBSDataStore
 }
 
-func NewPBSDataStore(stores []PBSStore) *PBSDataStore {
+func DoMarshalling(value interface{}) (key string, data byte[], err os.Error) {
+	data, err := proto.Marshal(value)
+	if err == nil {
+		h := sha256.New()
+		nr, err := h.Write([]byte(data))
+		if err == nil && nr == len([]byte(data)) {
+			key := hex.EncodeToString(h.Sum())
+		}
+	}
+	return
+}
+
+func NewPBSDataStore(stores []DataStore) *PBSDataStore {
 	ds := new(PBSDataStore)
-	ds.stores = stores
-	return ds
-}
-
-func NewPBSMemoryStore() *PBSMemoryStore {
-	ds := new(PBSMemoryStore)
-	ds.kvstore = make(map[string][]byte)
-	return ds
-}
-
-func NewPBSDiskStore(filename string) *PBSDiskStore {
-	ds := new(PBSDiskStore)
-	ds.keys = make([]string)
-	ds.filename = filename
-	return ds
-}
-
-func NewPBSNetworkStore(hostname string) *PBSNetworkStore {
-	ds := new(PBSNetworkStore)
-	ds.keys = make([]string)
-	ds.hostname = hostname
+	ds.prev = nil
+	ds.DataStore = stores[0]
+	stores[0].PBSDataStore = ds
+	ds.keys = make(map[string] PBSDataStore)
+	prev = ds
+	for store in stores[1:] {
+		ns := new(PBSDataStore)
+		prev.next = ns
+		ns.prev = prev
+		ns.DataStore = store
+		store.PBSDataStore = ns
+		prev = ns
+	}
 	return ds
 }
 
 func (ds *PBSDataStore) Put(value interface{}) (key string, err os.Error) {
+	t_key, data, err := DoMarshalling(value)
+	if err == nil && ds.keys[t_key] == nil {
+		err = ds.PBSStore.Put(t_key, data)
+		if err == nil {
+			ds.keys[t_key] = ds.PBSStore
+			key = t_key
+		}
+	}
+	return
+}
+
+func (ds *PBSDataStore) Get(key string, value interface{}) (err os.Error) {
+	
+}
+
+func (ds *PBSDataStore) Delete(key string) (err os.Error) {
+	
+}
+
+func (ds *PBSDataStore) Push(key string) (err os.Error) {
+	
+}
+
+func (ds *PBSDataStore) Pull(key string) (err os.Error) {
+
+}
+
+func NewMemoryStore() *MemoryStore {
+	ds := new(MemoryStore)
+	ds.kvstore = make(map[string][]byte)
+	return ds
+}
+
+func (ds *PBSMemoryStore) Put(value interface{}) (key string, err os.Error) {
 	data, err := proto.Marshal(value)
 	if err == nil {
 		h := sha256.New()
@@ -77,9 +136,24 @@ func (ds *PBSDataStore) Put(value interface{}) (key string, err os.Error) {
 	return
 }
 
-func (ds *PBSDataStore) Get(key string, value interface{}) (err os.Error) {
+func (ds *PBSMemoryStore) Get(key string, value interface{}) (err os.Error) {
 	if ds.kvstore[key] != nil {
 		err = proto.Unmarshal(ds.kvstore[key], value)
 	}
 	return
 }
+
+func NewPBSDiskStore(filename string) *PBSDiskStore {
+	ds := new(PBSDiskStore)
+	ds.keys = make([]string)
+	ds.filename = filename
+	return ds
+}
+
+func NewPBSNetworkStore(hostname string) *PBSNetworkStore {
+	ds := new(PBSNetworkStore)
+	ds.keys = make([]string)
+	ds.hostname = hostname
+	return ds
+}
+
