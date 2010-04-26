dceb289cf9c2fdd06e98e1114ea8d167638cfc25
Michael Dietz <mdietz@gmail.com>
2010-04-26 03:09:46 -0500

    Adding in code to handle expiration of kv pairs to fallback stores

--- pbs.go
+++ pbs.go
@@ -7,18 +7,55 @@ import (
 	"encoding/hex"
 )
 
-type DataStore interface{
+type PBSStore interface{
 	Put(value interface{}) (key string, err os.Error)
 	Get(key string, value interface{}) (err os.Error)
+	Delete(key string) (err os.Error)
+	Push(key string) (err os.Error)
+	Pull(key String) (err os.Error)
 }
-
-type PBSDataStore struct{
+		
+type PBSMemoryStore struct{
 	kvstore map[string][]byte
 }
 
-func NewPBS() *PBSDataStore {
+type PBSDiskStore struct{
+	keys []string
+	filename string
+}
+
+type PBSNetworkStore struct{
+	keys []string
+	hostname string
+}
+
+type PBSDataStore struct{
+	stores []PBSStore
+}
+
+func NewPBSDataStore(stores []PBSStore) *PBSDataStore {
 	ds := new(PBSDataStore)
+	ds.stores = stores
+	return ds
+}
+
+func NewPBSMemoryStore() *PBSMemoryStore {
+	ds := new(PBSMemoryStore)
 	ds.kvstore = make(map[string][]byte)
+	return ds
+}
+
+func NewPBSDiskStore(filename string) *PBSDiskStore {
+	ds := new(PBSDiskStore)
+	ds.keys = make([]string)
+	ds.filename = filename
+	return ds
+}
+
+func NewPBSNetworkStore(hostname string) *PBSNetworkStore {
+	ds := new(PBSNetworkStore)
+	ds.keys = make([]string)
+	ds.hostname = hostname
 	return ds
 }
 
